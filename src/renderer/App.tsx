f3a2a673698670ca9c8ffe1c710f70341308b0ea
Wai Yan Phyo <oyhpnayiaw@gmail.com>
2025-12-19 00:10:43 +0700

    Init

---
+++ src/renderer/App.tsx
@@ -0,0 +1,15 @@
+import { GeckoUIPortal } from '@geckoui/geckoui';
+
+function App() {
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">RBAC Scanner</h1>
+        <p className="text-gray-600">Welcome to your application.</p>
+      </div>
+      <GeckoUIPortal />
+    </>
+  );
+}
+
+export default App;
